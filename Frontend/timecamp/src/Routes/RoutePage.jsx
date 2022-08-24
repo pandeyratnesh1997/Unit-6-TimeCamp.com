0c44ea39b0c6926d0802977f664977fb4a04087c
Suraj Badwaik <101567697+Suraj-Badwaik@users.noreply.github.com>
2022-08-24 10:04:24 +0530

    top nav and side bar

--- Frontend/timecamp/src/Routes/RoutePage.jsx
+++ Frontend/timecamp/src/Routes/RoutePage.jsx
@@ -1,10 +1,16 @@
 // write all route link here and add on app.js
 
 import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Sidebar from '../Components/Sidebar'
+import TopNav from '../Components/TopNav'
 
 const RoutePage = () => {
   return (
-    <div>RoutePage</div>
+    <>
+      <TopNav/>
+      <Sidebar/>
+    </>
   )
 }
 
