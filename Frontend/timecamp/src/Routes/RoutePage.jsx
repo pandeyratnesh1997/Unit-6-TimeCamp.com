b11a27b9a8ff27894780e16cff72fb51c185df5c
Harapriyasahu <99711894+Harapriyasahu@users.noreply.github.com>
2022-08-25 17:41:15 +0530

    day3

--- Frontend/timecamp/src/Routes/RoutePage.jsx
+++ Frontend/timecamp/src/Routes/RoutePage.jsx
@@ -5,12 +5,15 @@ import {Routes, Route} from "react-router-dom"
 import Footer from '../Components/Footer'
 import Sidebar from '../Components/Sidebar'
 import TopNav from '../Components/TopNav'
+import { Homepage } from '../Pages/Hompage'
+
 
 const RoutePage = () => {
   return (
 
     <>
       <TopNav/>
+      <Homepage/>
       <Footer/>
     </>
 
