ca306d211bd7f818d3235d26fb0d912e990238ea
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-08-28 16:35:59 +0530

    users and attendance

--- Frontend/timecamp/src/Pages/Users.jsx
+++ Frontend/timecamp/src/Pages/Users.jsx
@@ -9,8 +9,8 @@ import {GrAdd} from 'react-icons/gr'
 import { FiSettings } from 'react-icons/fi'
 import {GrCircleQuestion} from 'react-icons/gr';
 import {BsHourglassTop} from 'react-icons/bs';
-import { SimpleGrid } from '@chakra-ui/react'
-import { useMediaQuery } from '@chakra-ui/react';
+import { SimpleGrid,  } from '@chakra-ui/react'
+import { useMediaQuery,Spacer } from '@chakra-ui/react';
 
 
 
@@ -20,7 +20,7 @@ import { useMediaQuery } from '@chakra-ui/react';
 const Users = () => {
   return (
     
-    <Box boxSize={'border-box'} border='1px solid rgb(219,219,219)' top={'50%'} height={'60px'} ml='500' w='1000px'>
+    <Box boxSize={'border-box'} border='1px solid rgb(219,219,219)' top={'60%'} height={'60px'} ml='500' w='1000px' marginLeft={'250px'}>
    
    <Box bg='whitesmoke' w='100%' p={4} color='black'>
     <Button ml={'8'}><BsHourglassTop/></Button>
@@ -30,31 +30,30 @@ const Users = () => {
     </Box>
 
    
-    <Container mb={'20'}>
-    <HStack justifyContent={'space-between'} align='right'>
-            <Text fontSize={'20'} fontWeight="700" >Users</Text>
-            <Box gap='15px'>
-            <Button ml={'8'}><FiSettings/></Button>
-            <Button ml={'8'}><GrCircleQuestion/></Button>
-            <Button ml={'8'}><IoPersonAddOutline/></Button>
-            <Button ml={'8'}><CgProfile/></Button>
-            </Box>
-
-        </HStack>
-    </Container>
+    <Flex w='100%' justifyContent={'space-between'}>
+    <Box p='2' bg='white'>
+    <b>Users</b>
+    </Box>
+    <Spacer />
+  
+            <Button ml={'8'} bg='white'><FiSettings/></Button>
+            <Button ml={'8'} bg='white'><GrCircleQuestion/></Button>
+            <Button ml={'8'} bg='white'><IoPersonAddOutline/></Button>
+            <Button ml={'8'} bg='white'><CgProfile/></Button>
+</Flex>
     <Box bg='rgb(216,233,255)' w='100%' p={4} color='rgb(65,147,248)'>
-    <Button ml={'8'}><IoPersonAddOutline/></Button>
-    You can now invite guests like clients, vendors, or contractors. You can add them free of charge. Invite guest
+    <Button ml={'8'} bg='rgb(216,233,255)'><IoPersonAddOutline/></Button>
+    You can now invite guests like clients, vendors, or contractors. You can add them free of charge.<b>Invite guest</b>
 </Box>
         <Box mb='15'>
-            <HStack spacing={'2'} >
+            <HStack spacing={'2'} justifyContent={'space-between'} >
            
                 <Button colorScheme='green' size='md' w={'111px'}>+Invite people</Button>
-                <Button colorScheme='green' size='md' w={'151px'}>Create User Group</Button>
+                <Button colorScheme='white' color='black' border='1px solid gray' size='md' w={'151px'}>Create User Group</Button>
                 <Box  gap='15px'>
             <Button ml={'8'}><HiOutlineMail/></Button>
             </Box>
-            <Box><p>change number of seats</p></Box>
+            <Box color='blue'><p>Change number of seats</p></Box>
             <Input placeholder='Search Users' w={'222px'} />
                        
             </HStack>
@@ -62,27 +61,26 @@ const Users = () => {
         <Box border='1px' h={'200px'} borderColor='gray.200'>
         <Checkbox defaultChecked>
         People (1 user)
-        <Button><FaRegEdit/></Button>
-        <Button><GrAdd/></Button>
+        <Button bg='white'><FaRegEdit/></Button>
+        <Button bg='white'><GrAdd/></Button>
                 </Checkbox>
 
-                <SimpleGrid columns={1} spacing={10}>
-  <Box bg='whitesmoke' height='80px'>
-  <Checkbox defaultChecked>
+  <SimpleGrid columns={1} spacing={10}>
+  <Box bg='whitesmoke' height='80px' border='1px solid whitesmoke'>
+  <Checkbox defaultChecked></Checkbox>
   <Button ml={'8'}><CgProfile/></Button>
         RatneshPandey(Account Owner)
-        
-                </Checkbox>
   </Box>
-  <Box bg='whitesmoke' height='80px'></Box>
+  
   
 </SimpleGrid>
    
       </Box>
+
         <Box>
-            <HStack gap={'2'}>
+            <HStack gap={'2'} justifyContent={'space-between'}>
                 <Select w='60' placeholder="Bulk Edit">
-                <option value='All tasks'>Delete Selected</option>
+                    <option value='All tasks'>Delete Selected</option>
                     <option value='My tasks'>Move Selected</option>
                     <option value='All tasks'>Change role for selected</option>
                     <option value='My tasks'>Disable/Enable Selected</option>
