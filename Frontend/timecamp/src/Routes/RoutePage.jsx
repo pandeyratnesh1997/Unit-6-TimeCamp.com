3b03be77a5c2859f34365589a975a2c8420ed506
Suraj Badwaik <101567697+Suraj-Badwaik@users.noreply.github.com>
2022-08-28 16:29:08 +0530

    sdf

--- Frontend/timecamp/src/Routes/RoutePage.jsx
+++ Frontend/timecamp/src/Routes/RoutePage.jsx
@@ -1,14 +1,20 @@
+
 // write all route link here and add on app.js
 
 
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Basicsub from "../Components/Basicsub";
+import Customsub from "../Components/Customsub";
+import Prosub from "../Components/Prosub";
+import Subcription from "../Components/Subcription";
 import Billing from "../Pages/Billing";
 import { Homepage } from "../Pages/Hompage";
 import Login from "../Pages/Login";
 import Projectpage from "../Pages/Projectpage";
 import Signin from "../Pages/Signin";
+import TaskEdit from "../Pages/TaskEdit";
 import TimeSheet from "../Pages/TimeSheet";
 
 
@@ -21,11 +27,13 @@ const RoutePage = () => {
         <Route path="/" element={<Homepage/>}/>
         <Route path="/project" element={<Projectpage/>}/>
         <Route path="/project/task" element={<Projectpage/>}/>
+        <Route path="/project/task/:taskId/edit" element={<TaskEdit/>}/>
+        <Route path="/timesheet" element={<TimeSheet/>}/>
         <Route path="/billing" element={<Billing/>}/>
-        <Route path="/billing/subscription" element={<Billing/>}/>
-        <Route path="/billing/custom" element={<Billing/>}/>
-        <Route path="/billing/basics" element={<Billing/>}/>
-        <Route path="/billing/pro" element={<Billing/>}/>
+        <Route path="/billing/subscription" element={<Subcription/>}/>
+        <Route path="/billing/custom" element={<Customsub/>}/>
+        <Route path="/billing/basics" element={<Basicsub/>}/>
+        <Route path="/billing/pro" element={<Prosub/>}/>
         <Route path="/register" element={<Signin/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
@@ -38,4 +46,3 @@ const RoutePage = () => {
 
 
 export default RoutePage;
-
